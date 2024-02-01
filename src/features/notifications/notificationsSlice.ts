08b0b2ab85fa4e74429056dacc0b2a9db5aceead
Mark Erikson <mark@isquaredsoftware.com>
2024-01-31 23:09:44 -0500

    Add notifications endpoint with streaming websocket updates
    # Conflicts:
    #	src/features/notifications/notificationsSlice.ts

--- src/features/notifications/notificationsSlice.ts
+++ src/features/notifications/notificationsSlice.ts
@@ -1,9 +1,12 @@
-import { createEntityAdapter, createSlice } from '@reduxjs/toolkit'
+import { createEntityAdapter, createSelector, createSlice } from '@reduxjs/toolkit'
 
 import { client } from '@/api/client'
+import { forceGenerateNotifications } from '@/api/server'
 
-import type { RootState } from '@/app/store'
+import type { AppThunk, RootState } from '@/app/store'
 import { createAppAsyncThunk } from '@/app/withTypes'
+
+import { apiSlice } from '@/features/api/apiSlice'
 
 export interface ServerNotification {
   id: string
@@ -17,12 +20,74 @@ export interface ClientNotification extends ServerNotification {
   isNew: boolean
 }
 
-export const fetchNotifications = createAppAsyncThunk('notifications/fetchNotifications', async (_unused, thunkApi) => {
-  const allNotifications = selectAllNotifications(thunkApi.getState())
+export const extendedApi = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getNotifications: builder.query<ServerNotification[], void>({
+      query: () => '/notifications',
+      async onCacheEntryAdded(arg, { updateCachedData, cacheDataLoaded, cacheEntryRemoved }) {
+        // create a websocket connection when the cache subscription starts
+        const ws = new WebSocket('ws://localhost')
+        try {
+          // wait for the initial query to resolve before proceeding
+          await cacheDataLoaded
+
+          // when data is received from the socket connection to the server,
+          // update our query result with the received message
+          const listener = (event: any) => {
+            const message: { type: 'notifications'; payload: ServerNotification[] } = JSON.parse(event.data)
+            switch (message.type) {
+              case 'notifications': {
+                updateCachedData((draft) => {
+                  // Insert all received notifications from the websocket
+                  // into the existing RTKQ cache array
+                  draft.push(...message.payload)
+                  draft.sort((a, b) => b.date.localeCompare(a.date))
+                })
+                break
+              }
+              default:
+                break
+            }
+          }
+
+          ws.addEventListener('message', listener)
+        } catch {
+          // no-op in case `cacheEntryRemoved` resolves before `cacheDataLoaded`,
+          // in which case `cacheDataLoaded` will throw
+        }
+        // cacheEntryRemoved will resolve when the cache subscription is no longer active
+        await cacheEntryRemoved
+        // perform cleanup steps once the `cacheEntryRemoved` promise resolves
+        ws.close()
+      },
+    }),
+  }),
+})
+
+export const { useGetNotificationsQuery } = extendedApi
+
+const emptyNotifications: ServerNotification[] = []
+
+export const selectNotificationsResult = extendedApi.endpoints.getNotifications.select()
+
+const selectNotificationsData = createSelector(
+  selectNotificationsResult,
+  (notificationsResult) => notificationsResult.data ?? emptyNotifications,
+)
+
+export const fetchNotificationsWebsocket = (): AppThunk => (dispatch, getState) => {
+  const allNotifications = selectNotificationsData(getState())
+  const [latestNotification] = allNotifications
+  const latestTimestamp = latestNotification?.date ?? ''
+  // Hardcode a call to the mock server to simulate a server push scenario over websockets
+  forceGenerateNotifications(latestTimestamp)
+}
+
+export const fetchNotifications = createAppAsyncThunk('notifications/fetchNotifications', async (_, { getState }) => {
+  const allNotifications = selectAllNotifications(getState())
   const [latestNotification] = allNotifications
   const latestTimestamp = latestNotification ? latestNotification.date : ''
   const response = await client.get<ServerNotification[]>(`/fakeApi/notifications?since=${latestTimestamp}`)
-
   return response.data
 })
 
